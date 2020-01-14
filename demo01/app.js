bc5a21c8e990d3344e35824e7ac525c312521be7
huangli <huangli@souche.com>
2020-01-14 21:02:24 +0800

    增加第一个例子的注释

--- demo01/app.js
+++ demo01/app.js
@@ -1,5 +1,7 @@
 /**
  * webpack支持ES6、CommonJs和AMD规范
+ * 
+ * 
  */
 
 // ES6
@@ -11,6 +13,6 @@ var minus = require("./vendor/minus");
 console.log("minus(1, 2) = ", minus(1, 2));
 
 // AMD
-require(["./vendor/multi"], function(multi) {
-  console.log("multi(1, 2) = ", multi(1, 2));
+require(["./vendor/multi"], function (multi) {
+    console.log("multi(1, 2) = ", multi(1, 2));
 });
--- demo01/webpack.config.js
+++ demo01/webpack.config.js
@@ -1,17 +1,20 @@
 /**
  * 必须使用CommonJs规范
  * 更多高级用法：https://www.webpackjs.com/concepts/output/
+ * 
+ *  先安装全局webpack,webpack-cli
+ *  再运行： webpack --watch 命令来执行
  */
 
 const path = require("path");
 
 module.exports = {
-  entry: {
-    app: "./app.js"
-  },
-  output: {
-    publicPath: __dirname + "/dist/", // js引用路径或者CDN地址
-    path: path.resolve(__dirname, "dist"), // 打包文件的输出目录
-    filename: "bundle.js"
-  }
+    entry: {
+        app: "./app.js"
+    },
+    output: {
+        publicPath: __dirname + "/dist/", // js引用路径或者CDN地址
+        path: path.resolve(__dirname, "dist"), // 打包文件的输出目录
+        filename: "bundle.js"
+    }
 };
